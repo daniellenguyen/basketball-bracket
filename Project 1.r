b2a469d08dd90436d24b7bc6c223d2f838882f27
Dan <cohen.dan@husky.neu.edu>
2016-09-23 14:47:49 -0400

    used plotly and igraph to display networks

--- Project 1.r
+++ Project 1.r
@@ -1,11 +1,53 @@
 require(shiny)
-# require(plotly)
+require(plotly)
+require(igraph)
 
-bracket = read.csv("NCAA data.csv", na.strings="")
+doLayout = function(G, layoutFunc, title = "") {
+	L = layoutFunc(G)
+	Xn = L[,1]
+	Yn = L[,2]
+	vs = V(G)
+	es = as.data.frame(get.edgelist(G))
+	
+	network = plot_ly(type = "scatter", x = Xn, y = Yn, mode = "markers",
+						text = vs$label, hoverinfo = "text")
 
-ui = fluidPage(tableOutput("showDF"))
-server = function(input, output) {
-	output$showDF = renderTable(bracket)
+	edgeShapes = list()
+	for (i in (1:length(es[1]$V1))) {
+		v1 = es[i,]$V1
+		v2 = es[i,]$V2
+
+		edgeShapes[[i]] = list(
+			type = "line",
+			line = list(color = "#030303", width = 0.3),
+			x0 = Xn[v1],
+			y0 = Yn[v1],
+			x1 = Xn[v2],
+			y1 = Yn[v2]
+		)
+	}
+
+	layout(network, title = title, shapes = edgeShapes,
+			xaxis = list(title = "", showgrid = FALSE, showticklabels = FALSE, zeroline = FALSE),
+			yaxis = list(title = "", showgrid = FALSE, showticklabels = FALSE, zeroline = FALSE)
+		)
 }
+
+G = read.graph("test_graph.gml", format = c("gml"))
+
+# shiny
+ui = fluidPage(
+		titlePanel("Test Graph"),
+		plotlyOutput("fr"),
+		plotlyOutput("circle")
+		# plotlyOutput("tree")
+	)
+
+server = function(input, output) {
+	output$fr     = renderPlotly({ doLayout(G, layout.fruchterman.reingold, title = "Fruchterman Reingold") })
+	output$circle = renderPlotly({ doLayout(G, layout.circle,               title = "Circle") })
+	# output$tree = renderPlotly({ doLayout(G, layout_as_tree, title = "Tree") })
+}
+
 
 shinyApp(ui = ui, server = server)
