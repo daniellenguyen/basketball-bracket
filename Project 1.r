3b700ce1a9c8c85c6d9fbaf618609386cb63abc6
Dan <cohen.dan@husky.neu.edu>
2016-09-28 13:06:28 -0400

    show direction of edges and show more height

--- Project 1.r
+++ Project 1.r
@@ -9,12 +9,13 @@ es = G %>% get.edgelist %>% as.data.frame
 colnames(es) = c("from", "to")
 
 server = function(input, output) {
-	output$default = renderVisNetwork(visNetwork(vs, es, height = "100%"))
+	output$default = renderVisNetwork(
+		visNetwork(vs, es, width = "100%") %>% visEdges(arrows = "from"))
 }
 
 ui <- fluidPage(
 	titlePanel("This is a title"),
-    visNetworkOutput("default")
+    visNetworkOutput("default", height = "900px")
 )
 
 shinyApp(ui = ui, server = server)
