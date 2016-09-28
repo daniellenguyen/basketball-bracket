3c32ff44df81cc0e809e2430a8d0ac5488565d0e
Dan <cohen.dan@husky.neu.edu>
2016-09-28 12:51:45 -0400

    added labels

--- Project 1.r
+++ Project 1.r
@@ -4,12 +4,12 @@ require(visNetwork)
 
 G = read.graph("graph.gml", format = c("gml"))
 vs = V(G)
-vs = data.frame(id = vs$name, region = vs$label)
+vs = data.frame(id = vs$name, label = vs$name, region = vs$label)
 es = G %>% get.edgelist %>% as.data.frame
 colnames(es) = c("from", "to")
 
 server = function(input, output) {
-	output$default = renderVisNetwork(visNetwork(vs, es))
+	output$default = renderVisNetwork(visNetwork(vs, es, height = "100%"))
 }
 
 ui <- fluidPage(
