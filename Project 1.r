b432d29cec61241338db313446b4ab1afd5635d5
Dan <cohen.dan@husky.neu.edu>
2016-09-28 21:04:10 -0400

    made the graph prettier

--- Project 1.r
+++ Project 1.r
@@ -4,18 +4,29 @@ require(visNetwork)
 
 G = read.graph("graph.gml", format = c("gml"))
 vs = V(G)
-vs = data.frame(id = vs$name, label = vs$name, region = vs$label)
+vs = data.frame(id = vs$name, label = vs$name, group = vs$label)
 es = G %>% get.edgelist %>% as.data.frame
 colnames(es) = c("from", "to")
 
+regions = vs[['group']]
+
 server = function(input, output) {
 	output$default = renderVisNetwork(
-		visNetwork(vs, es, width = "100%") %>% visEdges(arrows = "from"))
+		visNetwork(vs, es, width = "100%") %>% 
+		visEdges(arrows = "from") %>%
+		visGroups %>%
+		visLegend
+	)
 }
 
 ui <- fluidPage(
-	titlePanel("This is a title"),
+	titlePanel("NCAA Teams"),
     visNetworkOutput("default", height = "900px")
 )
 
 shinyApp(ui = ui, server = server)
+
+# group by
+# 	- region
+# 	- public/private
+#	- tuition
